533134e6c2a60f85e1e5e10fe5c260e3a0c1be41
DươngLondon <ndd050903@gmail.com>
2023-10-01 22:05:49 +0700

    remove

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = "localhost";//địa chỉ my sql sever kết nối đến
-$dbname = "nguoi_dung";//tên database kết nối đến
-$username = "root";//tên username để kết nối tới database
-$password = "";//mật khẩu để kết nối đến database;
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",$username,$password);
-?>
--- user_management.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Bao ve kiem soat
-session_start();
-//Bao ve kiem sait ra vao
-if(!$_SESSION['isLogin']){
-    header("Location:Login.php");
-}
-?>
